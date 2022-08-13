0668b40cda0dd091e18d9c2945766a86c6a4fe62
mattgaither <benjamin.gaither@shipt.com>
2022-08-13 18:51:10 -0500

    added the index.db file and added the file to the service worker cache

--- Develop/public/js/service-worker.js
+++ Develop/public/js/service-worker.js
@@ -10,6 +10,7 @@ const FILES_TO_CACHE = [
     '/manifest.json',
     '/css/style.css',
     '/js/index.js',
+    '/js/indexdb.js',
     '/icons/icon-72x72.png',
     '/icons/icon-96x96.png',
     '/icons/icon-128x128.png',
